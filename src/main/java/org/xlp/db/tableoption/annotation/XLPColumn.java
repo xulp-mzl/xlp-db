c89ab1f3efe46ba14bf9c9ce403fd411f2afce4b
xulongp <15179220203@163.com>
2021-03-15 22:02:49 +0800

    添加注解内容

--- src/main/java/org/xlp/db/tableoption/annotation/XLPColumn.java
+++ src/main/java/org/xlp/db/tableoption/annotation/XLPColumn.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.xlp.db.tableoption.xlpenum.DataType;
+import org.xlp.utils.XLPStringUtil;
+
 /**
  * 用来判断是否数据库表中的字段
  * 
@@ -32,4 +35,34 @@ public @interface XLPColumn {
 	 */
 	public boolean trim() default true;
 	
+	//以下字段用来根据实体类创建相应的表
+	/**
+	 * 字段长度
+	 */
+	public int length() default -1;
+	
+	/**
+	 * 小数部分长度
+	 */
+	public int decimalLength() default 0;
+	
+	/**
+	 * 默认字符串值
+	 */
+	public String defaultValue() default XLPStringUtil.NULL_STRING;
+	
+	/**
+	 * 数据类型
+	 */
+	public DataType dataType() default DataType.NONE;
+	
+	/**
+	 * 是否可为空值，默认可以 
+	 */
+	public boolean isNull() default true;
+	
+	/**
+	 * 默认值是否用0填充
+	 */
+	public boolean zeroFill() default false;
 }
---
+++ src/main/java/org/xlp/db/tableoption/xlpenum/DataType.java
@@ -0,0 +1,48 @@
+package org.xlp.db.tableoption.xlpenum;
+
+import org.xlp.utils.XLPStringUtil;
+
+/**
+ * <p>创建时间：2021年3月14日 下午10:48:06</p>
+ * @author xlp
+ * @version 1.0 
+ * @Description 数据库表字段的类型
+*/
+public enum DataType {
+	BIGINT("bigint"),
+	BLOB("blob"),
+	CHAR("char"),
+	INT("int"),
+	DATETIME("datetime"),
+	DECIMAL("decimal"),
+	DOUBLE("double"),
+	ENUM("enum"),
+	FLOAT("float"),
+	LONGBLOB("longblob"),
+	LONGTEXT("longtext"),
+	MEDIUMBLOB("mediumblob"),
+	MEDIUMTEXT("mediumtext"),
+	SET("set"),
+	SMALLINT("smallint"),
+	TEXT("text"),
+	TIME("time"),
+	TIMESTAMP("timestamp"),
+	BOOLEAN("boolean"),
+	BOOL("bool"),
+	VARCHAR("varchar"),
+	TINYINT("tinyint"),
+	NONE(XLPStringUtil.EMPTY)
+	;
+	private String dataTypeName;
+	
+	private DataType(String dataTypeName){
+		this.dataTypeName = dataTypeName;
+	}
+
+	/**
+	 * @return 数据库表字段的类型名称
+	 */
+	public String getDataTypeName() {
+		return dataTypeName;
+	}
+}
