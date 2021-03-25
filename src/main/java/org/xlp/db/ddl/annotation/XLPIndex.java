b345cad180601f0f15b00cfe89f94f1e8589fff7
xulongp <15179220203@163.com>
2021-03-25 22:34:54 +0800

    修改XLPIndex注解的作用域

--- src/main/java/org/xlp/db/ddl/annotation/XLPIndex.java
+++ src/main/java/org/xlp/db/ddl/annotation/XLPIndex.java
@@ -20,7 +20,7 @@ import org.xlp.utils.XLPStringUtil;
  * 
  */
 @Retention(RetentionPolicy.RUNTIME) // 注解会在class字节码文件中存在，在运行时可以通过反射获取到  
-@Target({ElementType.TYPE})//定义注解的作用目标**作用范围字段 
+@Target({ElementType.FIELD})//定义注解的作用目标**作用范围字段 
 @Documented//说明该注解将被包含在javadoc中 
 public @interface XLPIndex {
 	/**
